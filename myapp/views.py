135a64712bb1109a26c630d492f922600d928184
egoing <egoing@gmail.com>
2021-12-06 15:14:30 +0900

    7.READ.홈페이지 만들기

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, HttpResponse
-import random
 
-# Create your views here.
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'},
+]
+
 def index(request):
-    return HttpResponse('<h1>Random</h1>'+str(random.random()))
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return HttpResponse(f'''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        <h2>Welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''')
 
 def create(request):
     return HttpResponse('Create')
