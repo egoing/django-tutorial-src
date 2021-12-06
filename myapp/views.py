e294ebd8aaefdf9378ff19aa43f55c7960366cdf
egoing <egoing@gmail.com>
2021-12-06 13:34:38 +0900

    6.django를 쓰는 이유

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse
+import random
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome!')
+    return HttpResponse('<h1>Random</h1>'+str(random.random()))
 
 def create(request):
     return HttpResponse('Create')
