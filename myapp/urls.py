ea5232c914c1b1d70343084fe7a7b9412992b564
egoing <egoing@gmail.com>
2021-12-12 00:15:43 +0900

    12.Update

--- myapp/urls.py
+++ myapp/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('', views.index),
     path('create/', views.create),
     path('read/<id>/', views.read),
+    path('update/<id>/', views.update),
     path('delete/', views.delete)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -19,6 +19,7 @@ def HTMLTemplate(articleTag, id=None):
                     <input type="submit" value="delete">
                 </form>
             </li>
+            <li><a href="/update/{id}">update</a></li>
         '''
     ol = ''
     for topic in topics:
@@ -74,6 +75,35 @@ def create(request):
         url = '/read/'+str(nextId)
         nextId = nextId + 1
         return redirect(url)
+
+@csrf_exempt
+def update(request,id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic = {
+                    "title":topic['title'],
+                    "body":topic['body']
+                }
+        article = f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]}></p>
+                <p><textarea name="body" placeholder="body">{selectedTopic['body']}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article, id))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body
+        return redirect(f'/read/{id}')
+
+
 
 @csrf_exempt
 def delete(request):
